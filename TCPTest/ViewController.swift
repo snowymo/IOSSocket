a4e344f2559f263e9fef0703180c5715abbeab56
Zhenyi He <snowymohehe@gmail.com>
2019-02-01 17:02:58 -0500

    add ip text field, udp/tcp and sending switch for more controls

--- TCPTest/ViewController.swift
+++ TCPTest/ViewController.swift
@@ -15,18 +15,31 @@ class ViewController: UIViewController {
     
     let host = "10.19.247.30"
     let port = 12345
-    var client: TCPClient?
+    var tcpClient: TCPClient?
     var udpClient: UDPClient?
     
     var interval = 0.0001
+    
+    @IBOutlet weak var TextIPAddress: UITextField!
+    var curProtocol = "udp"
+    var curSending = false
     
     var motionManager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        client = TCPClient(address: host, port: Int32(port))
+        tcpClient = TCPClient(address: host, port: Int32(port))
         udpClient = UDPClient(address: host, port: Int32(port))
+    }
+    @IBAction func IPChanged(_ sender: UITextField) {
+        if self.curProtocol == "udp"{
+            self.udpClient = UDPClient(address: sender.text!, port: Int32(self.port))
+        }
+        else{
+            self.tcpClient = TCPClient(address: sender.text!, port: Int32(self.port))
+        }
+        print("Connected to host \(sender.text ?? "none")")
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -35,11 +48,24 @@ class ViewController: UIViewController {
             if let mydata = data{
                 print( mydata.rotationRate)
                 
-                self.appendToTextField(string: "Connected to host \(self.udpClient!.address)")
-                if let response = self.sendRequest(imu: mydata, using: self.udpClient!) {
-                    self.appendToTextField(string: "Response: \(response)")
+                if self.curSending{
+                    if self.curProtocol == "udp"{
+                        self.sendRequest(imu: mydata, using: self.udpClient!)
+                    }
+                    else{
+                        self.sendRequest(imu: mydata, using: self.tcpClient!)
+                    }
                 }
             }
+        }
+    }
+    
+    @IBAction func ProtocolChanged(_ sender: UISwitch) {
+        if sender.isOn{
+            // udp
+            curProtocol = "udp"
+        }else{
+            curProtocol = "tcp"
         }
     }
     
@@ -48,28 +74,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func UDPSend(_ sender: Any) {
-        guard let udpClient = udpClient else { return }
-
-            appendToTextField(string: "Connected to host \(udpClient.address)")
-            if let response = sendRequest(string: "GET / HTTP/1.0\n\n", using: udpClient) {
-                appendToTextField(string: "Response: \(response)")
-            }
-    }
-    
-    @IBAction func SendClick(_ sender: Any) {
-        guard let client = client else { return }
-        
-        switch client.connect(timeout: 10) {
-        case .success:
-            appendToTextField(string: "Connected to host \(client.address)")
-            if let response = sendRequest(string: "GET / HTTP/1.0\n\n", using: client) {
-                appendToTextField(string: "Response: \(response)")
-            }
-        case .failure(let error):
-            appendToTextField(string: String(describing: error))
+    @IBAction func SendingChanged(_ sender: UISwitch) {
+        if sender.isOn{
+            // send
+            curSending = true
+        }
+        else{
+            curSending = false
         }
     }
+    
+//    @IBAction func SendClick(_ sender: Any) {
+//        guard let client = tcpClient else { return }
+//
+//        switch client.connect(timeout: 10) {
+//        case .success:
+//            appendToTextField(string: "Connected to host \(client.address)")
+//            if let response = sendRequest(string: "GET / HTTP/1.0\n\n", using: client) {
+//                appendToTextField(string: "Response: \(response)")
+//            }
+//        case .failure(let error):
+//            appendToTextField(string: String(describing: error))
+//        }
+//    }
     
     private func sendRequest(string: String, using client: TCPClient) -> String? {
         appendToTextField(string: "Sending data ... ")
@@ -83,9 +110,25 @@ class ViewController: UIViewController {
         }
     }
     
+    private func sendRequest(imu: CMGyroData, using client: TCPClient){
+        appendToTextField(string: "Sending imu ... ")
+        let arrayx = toByteArray(imu.rotationRate.x)
+        let arrayy = toByteArray(imu.rotationRate.y)
+        let arrayz = toByteArray(imu.rotationRate.z)
+        let arrayt = toByteArray(imu.timestamp)
+        let arrays = arrayx + arrayy + arrayz + arrayt
+        
+        switch client.send(data: arrays) {
+        case .success:
+            print("Sending successfully")
+        case .failure(let error):
+            print(String(describing: error))
+        }
+    }
+    
     private func readResponse(from client: TCPClient) -> String? {
         guard let response = client.read(1024*10) else { return nil }
-        
+
         return String(bytes: response, encoding: .utf8)
     }
     
@@ -107,7 +150,7 @@ class ViewController: UIViewController {
         return withUnsafeBytes(of: &value) { Array($0) }
     }
     
-    private func sendRequest(imu: CMGyroData, using client: UDPClient) -> String? {
+    private func sendRequest(imu: CMGyroData, using client: UDPClient){
         appendToTextField(string: "Sending imu ... ")
         let arrayx = toByteArray(imu.rotationRate.x)
         let arrayy = toByteArray(imu.rotationRate.y)
@@ -117,20 +160,18 @@ class ViewController: UIViewController {
         
         switch client.send(data: arrays) {
         case .success:
-            appendToTextField(string: "Sending successfully")
-            return nil
+            print("Sending successfully")
         case .failure(let error):
-            appendToTextField(string: String(describing: error))
-            return nil
+            print(String(describing: error))
         }
     }
     
-    private func readResponse(from client: UDPClient) -> String? {
-        guard let response = client.recv(1024*10).0 else { return nil }
-        //let response = client.recv(1024*10)
-        
-        return String(bytes: response, encoding: .utf8)
-    }
+//    private func readResponse(from client: UDPClient) -> String? {
+//        guard let response = client.recv(1024*10).0 else { return nil }
+//        //let response = client.recv(1024*10)
+//
+//        return String(bytes: response, encoding: .utf8)
+//    }
     
     private func appendToTextField(string: String) {
         print(string)
